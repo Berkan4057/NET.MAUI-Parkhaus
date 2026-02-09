0a043662556af6dc301c2ed2f6f585e32758a07d
Berkan4057 <berkan.ademi@icloud.com>
2026-02-09 18:59:20 +0100

    Ticket created

---
+++ ParkhausApp/Ticket.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ParkhausApp
+{
+    internal class Ticket
+    {
+        public Ticket() { }
+
+        public int TicketNumber { get; set; }
+        public decimal Price { get; set; }
+        public DateTime EntryTime { get; set; }
+        public bool IsPaid { get; set; }
+
+
+
+        }
+        
+        
+    }
+
