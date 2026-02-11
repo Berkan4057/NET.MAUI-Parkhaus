0014420a9881054c1fef8a6551b510cd503ae4cb
Berkan4057 <berkan.ademi@icloud.com>
2026-02-11 19:30:38 +0100

    Current Park Price in Dashboard page added

--- ParkhausApp/Dashboard.xaml.cs
+++ ParkhausApp/Dashboard.xaml.cs
@@ -14,7 +14,9 @@ public partial class Dashboard : ContentPage
 	{
 		base.OnAppearing();
 		StartParkingTimer();
+		CurrentPrice();
 	}
+
 
 	public void StartParkingTimer()
 	{
@@ -30,5 +32,18 @@ public partial class Dashboard : ContentPage
 
 		});
 		
+    }
+	public void CurrentPrice()
+	{ 
+	Dispatcher.StartTimer(TimeSpan.FromSeconds(1), () =>
+		{
+            var Time = DateTime.Now - _ticket.EntryTime;
+            var TimeMinute = Math.Floor(Time.TotalMinutes);
+            double price = TimeMinute / 1 * 2;
+            CurrentPriceLabel.Text = $"Aktuelle Preis: {price} CHF";
+            return true;
+
+        });
+
     }
 }
